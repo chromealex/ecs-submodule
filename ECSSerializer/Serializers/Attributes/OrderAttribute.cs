d75513575de989ee11b36caa9707beb0bfb35eaa
Alex Silaev <chrome.alex@gmail.com>
2020-12-09 16:48:20 +0300

    Code style fixes

--- ECSSerializer/Serializers/Attributes/OrderAttribute.cs
+++ ECSSerializer/Serializers/Attributes/OrderAttribute.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace ME.ECS.Serializer.Attributes
-{
-	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-	public sealed class OrderAttribute : Attribute
-	{
-		private readonly int order_;
-		public OrderAttribute([CallerLineNumber] int order = 0)
-		{
-			order_ = order;
-		}
+namespace ME.ECS.Serializer.Attributes {
 
-		public int Order { get { return order_; } }
-	}
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class OrderAttribute : Attribute {
+
+        public int order;
+
+        public OrderAttribute([CallerLineNumber] int order = 0) {
+            
+            this.order = order;
+            
+        }
+
+    }
+
 }
