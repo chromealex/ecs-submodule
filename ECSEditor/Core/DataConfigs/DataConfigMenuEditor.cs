6db5af8738b0812d1d507010e590dac60df09cc9
Alex Silaev <chrome.alex@gmail.com>
2020-12-29 11:41:58 +0300

    GetHash() random state added Data Config: editor menu added

---
+++ ECSEditor/Core/DataConfigs/DataConfigMenuEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace ME.ECSEditor {
+
+    public static class DataConfigUtils {
+
+        [MenuItem("ME.ECS/Data Configs/Force Update")]
+        public static void ForceUpdateConfigs() {
+            
+            AssetUtils.ForEachAsset<ME.ECS.DataConfigs.DataConfig>((path, config) => {
+                config.OnValidate();
+            });
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            
+        }
+        
+    }
+    
+}
