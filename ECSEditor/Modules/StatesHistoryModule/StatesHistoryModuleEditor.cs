4adde5d3dbc1730d049bc71d5de035f3adb90f28
Alex Silaev <chrome.alex@gmail.com>
2021-01-21 13:36:45 +0300

    Print events button added

--- ECSEditor/Modules/StatesHistoryModule/StatesHistoryModuleEditor.cs
+++ ECSEditor/Modules/StatesHistoryModule/StatesHistoryModuleEditor.cs
@@ -32,6 +32,21 @@ namespace ME.ECSEditor {
             UnityEngine.GUILayout.Label("<b>Events Added:</b> " + this.target.GetEventsAddedCount().ToString(), style);
             UnityEngine.GUILayout.Label("<b>Events Played:</b> " + this.target.GetEventsPlayedCount().ToString(), style);
 
+            if (UnityEngine.GUILayout.Button("Print Events") == true) {
+
+                foreach (System.Collections.DictionaryEntry ren in this.target.GetData()) {
+                    
+                    var entry = (ME.ECS.Collections.SortedList<long, HistoryEvent>)ren.Value;
+                    for (int i = 0; i < entry.Count; ++i) {
+
+                        UnityEngine.Debug.Log(entry[i].ToString());
+
+                    }
+                    
+                }
+
+            }
+
             var dataStates = this.target.GetDataStates();
             var entries = dataStates.GetEntries();
             foreach (var entryData in entries) {
