90f976d7925a001c92690736b45287a4cdb9040b
Alex Silaev <chrome.alex@gmail.com>
2021-04-16 21:33:42 +0300

    Added group tests for shared components

--- Tests/Tests/SharedComponents.cs
+++ Tests/Tests/SharedComponents.cs
@@ -189,6 +189,117 @@ namespace ME.ECS.Tests {
             
         }
 
+        [NUnit.Framework.TestAttribute]
+        public void SetGroup() {
+
+            this.TestDo((world) => {
+                
+                var entity = world.AddEntity();
+                entity.SetShared(new TestComponent() {
+                    data = 1,
+                }, 1);
+
+                var entity2 = world.AddEntity();
+                entity2.SetShared(new TestComponent() {
+                    data = 2,
+                });
+
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 1);
+                UnityEngine.Debug.Assert(entity2.ReadShared<TestComponent>().data == 2);
+                
+            });
+            
+        }
+
+        [NUnit.Framework.TestAttribute]
+        public void RemoveGroup() {
+
+            this.TestDo((world) => {
+                
+                var entity = world.AddEntity();
+                entity.SetShared(new TestComponent() {
+                    data = 1,
+                }, 1);
+
+                var entity2 = world.AddEntity();
+                entity2.SetShared(new TestComponent() {
+                    data = 1,
+                });
+                
+                entity2.RemoveShared<TestComponent>();
+                entity.RemoveShared<TestComponent>(1);
+
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 0);
+                UnityEngine.Debug.Assert(entity2.ReadShared<TestComponent>().data == 0);
+                
+            });
+            
+        }
+
+        [NUnit.Framework.TestAttribute]
+        public void GetGroup() {
+
+            this.TestDo((world) => {
+                
+                var entity = world.AddEntity();
+                entity.SetShared(new TestComponent() {
+                    data = 1,
+                }, 1);
+                
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 1);
+
+                ref var data = ref entity.GetShared<TestComponent>(1);
+                data.data = 2;
+
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 2);
+                
+            });
+            
+        }
+
+        [NUnit.Framework.TestAttribute]
+        public void GetOrCreateGroup() {
+
+            this.TestDo((world) => {
+                
+                var entity = world.AddEntity();
+                entity.SetShared(new TestComponent() {
+                    data = 1,
+                }, 1);
+                
+                var entity2 = world.AddEntity();
+                
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 1);
+
+                ref var data = ref entity2.GetShared<TestComponent>();
+                data.data = 2;
+
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 1);
+                UnityEngine.Debug.Assert(entity2.ReadShared<TestComponent>().data == 2);
+                
+            });
+            
+        }
+
+        [NUnit.Framework.TestAttribute]
+        public void ReadGroup() {
+
+            this.TestDo((world) => {
+                
+                var entity = world.AddEntity();
+                entity.SetShared(new TestComponent() {
+                    data = 1,
+                }, 1);
+                
+                var entity2 = world.AddEntity();
+                
+                UnityEngine.Debug.Assert(entity.ReadShared<TestComponent>(1).data == 1);
+                UnityEngine.Debug.Assert(entity2.ReadShared<TestComponent>().data == 0);
+                
+            });
+            
+        }
+
     }
 
 }
