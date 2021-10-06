8e0dd7ed24c7f615f31c2d33f3f2e48493e01256
Alex Silaev <chrome.alex@gmail.com>
2021-10-06 20:00:24 +0300

    Check over UI

--- ECSEssentials/Input/Utils/InputUtils.cs
+++ ECSEssentials/Input/Utils/InputUtils.cs
@@ -72,7 +72,9 @@
                 var touch = inp.GetTouch(index);
                 if (touch.phase == TouchPhase.Began) {
 
-                    return UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() == true || UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(touch.fingerId) == true;
+                    return UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() == true ||
+                           UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(touch.fingerId) == true ||
+                           UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject != null;
 
                 }
 
