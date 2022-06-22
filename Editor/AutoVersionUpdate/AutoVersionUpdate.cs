b446de84d32a7405831ff0d949bcf7419df674db
Alex Silaev <chrome.alex@gmail.com>
2022-06-22 21:22:29 +0300

    Editor: Auto CHANGELOG.md generator added

--- Editor/AutoVersionUpdate/AutoVersionUpdate.cs
+++ Editor/AutoVersionUpdate/AutoVersionUpdate.cs
@@ -9,26 +9,50 @@ namespace ME.ECSEditor {
     public static class AutoVersionUpdateCompilation {
 
         internal static UnityEditor.Compilation.Assembly[] assemblies;
+        internal static string currentVersion;
 
-        internal static void UpdatePackageVersion() {
+        internal static void UpdatePackageVersion(string[] filepaths) {
             
             var package = EditorUtilities.Load<TextAsset>("package.json", out var realPath);
             if (package != null) {
 
-                var source = package.text;
-                var pattern = @"""version"":\s*""(?<major>\d{1,2}).(?<minor>\d{1,2}).(?<build>\d{1,2})""";      
-                var result = Regex.Replace(source, pattern, AutoVersionUpdateCompilation.ReplaceEvaluator);
-                System.IO.File.WriteAllText(realPath, result);
+                var files = new string[filepaths.Length];
+                for (int i = 0; i < filepaths.Length; ++i) {
+                    files[i] = System.IO.Path.GetFileNameWithoutExtension(filepaths[i]);
+                }
 
+                var source = package.text;
+                var pattern = @"""version"":\s*""(?<major>\d{1,2}).(?<minor>\d{1,2}).(?<build>\d{1,2})""";
+                var result = Regex.Replace(source, pattern, AutoVersionUpdateCompilation.UpBuild);
+                ChangeLogEditorWindow.Create(files, currentVersion, (ver) => {
+                
+                    Debug.Log($"Version up to {ver}");
+                    if (currentVersion.StartsWith(ver) == true) {
+                        // major/minor not changed
+                        System.IO.File.WriteAllText(realPath, result);
+                    } else {
+                        // version changed
+                        newMajorMinorVersion = ver;
+                        var newResult = Regex.Replace(source, pattern, AutoVersionUpdateCompilation.SetMajorMinorVersion);
+                        System.IO.File.WriteAllText(realPath, newResult);
+                    }
+
+                });
+                
             }
 
         }
-        
-        private static string ReplaceEvaluator(System.Text.RegularExpressions.Match m) {
+
+        private static string newMajorMinorVersion;
+        private static string SetMajorMinorVersion(System.Text.RegularExpressions.Match m) {
+            return @"""version"": """ + AutoVersionUpdateCompilation.newMajorMinorVersion + ".0";
+        } 
+
+        private static string UpBuild(System.Text.RegularExpressions.Match m) {
             int major = int.Parse(m.Groups["major"].Value);
             int minor = int.Parse(m.Groups["minor"].Value);
             int build = int.Parse(m.Groups["build"].Value) + 1;
-            Debug.Log($"Version up to {major}.{minor}.{build}");
+            currentVersion = $"{major}.{minor}.{build}";
             return @"""version"": """ + major.ToString() + "." + minor.ToString() + "." + build.ToString() + @"""";
         } 
 
@@ -49,7 +73,7 @@ namespace ME.ECSEditor {
 
             if (AutoVersionUpdateCompilation.assemblies == null) AutoVersionUpdateCompilation.assemblies = UnityEditor.Compilation.CompilationPipeline.GetAssemblies();
 
-            //var assetReimport = string.Empty;
+            var assetReimport = new System.Collections.Generic.List<string>();
             var found = false;
             foreach (var asset in importedAssets) {
 
@@ -58,9 +82,8 @@ namespace ME.ECSEditor {
                     var assembly = AutoVersionUpdateCompilation.assemblies.FirstOrDefault(a => a.name.Contains("ME.ECS") == true && a.sourceFiles.Contains(asset) == true);
                     if (assembly != null) {
 
-                        //assetReimport = asset;
+                        assetReimport.Add(asset);
                         found = true;
-                        break;
                     
                     }
 
@@ -68,7 +91,7 @@ namespace ME.ECSEditor {
             
             }
             
-            if (found == true) AutoVersionUpdateCompilation.UpdatePackageVersion();
+            if (found == true) AutoVersionUpdateCompilation.UpdatePackageVersion(assetReimport.ToArray());
             
         }
 
---
+++ Editor/AutoVersionUpdate/ChangeLogEditorWindow.cs
@@ -0,0 +1,233 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace ME.ECSEditor {
+
+    public class ChangeLogEditorWindow : EditorWindow {
+
+        private const int FONT_SIZE = 12;
+
+        private const string linePrefix = "* ";
+        
+        private string version;
+        private string majorMinorVersion;
+        private string nextMajorMinorVersion;
+        private System.Action<string> callback;
+        private string changeLogFile;
+        private string changeLogFilePath;
+
+        private string changedFilename;
+        private string message;
+        
+        private GUIStyle fixedFontStyle;
+        private Font font;
+        
+        public static void Create(string[] changedFilenames, string version, System.Action<string> callback) {
+
+            var v = version.Split('.');
+            var majorMinor = v[0] + "." + v[1];
+            var nextMajorMinor = v[0] + "." + (int.Parse(v[1]) + 1);
+
+            for (int i = 0; i < changedFilenames.Length; ++i) {
+
+                var changedFilename = changedFilenames[i];
+                EditorUtilities.Load<TextAsset>("CHANGELOG.md", out var filePath);
+                var win = ChangeLogEditorWindow.CreateInstance<ChangeLogEditorWindow>();
+                win.message = changedFilename + ": ";
+                win.changedFilename = changedFilename + ": ";
+                win.version = version;
+                win.callback = callback;
+                win.nextMajorMinorVersion = nextMajorMinor;
+                win.majorMinorVersion = majorMinor;
+                win.changeLogFile = System.IO.File.ReadAllText(filePath);
+                win.changeLogFilePath = filePath;
+                win.ShowModal();
+
+            }
+
+        }
+
+        private void CleanupFont() {
+            
+            if (this.font == null) {
+                return;
+            }
+
+            Object.DestroyImmediate(this.font, true);
+            this.font = null;
+            
+        }
+
+        public void OnDisable() {
+            
+            this.CleanupFont();
+            
+        }
+
+        private void OnGUI() {
+
+            if (this.fixedFontStyle == null || this.font == null) {
+                
+                this.fixedFontStyle = new GUIStyle(GUI.skin.textArea);
+                var fontName = Application.platform == RuntimePlatform.WindowsEditor ? "Consolas" : "Courier";
+
+                this.CleanupFont();
+
+                this.font = Font.CreateDynamicFontFromOSFont(fontName, FONT_SIZE);
+                this.fixedFontStyle.normal = EditorStyles.label.normal;
+                this.fixedFontStyle.wordWrap = true;
+                this.fixedFontStyle.richText = true;
+                this.fixedFontStyle.font = this.font;
+                this.fixedFontStyle.fontSize = FONT_SIZE;
+                
+            }
+            
+            GUILayoutExt.Box(4f, 4f, () => {
+            
+                this.message = GUILayout.TextArea(this.message, this.fixedFontStyle, GUILayout.MinHeight(60f));
+
+                GUILayout.Space(10f);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button($"COMMIT TO {this.majorMinorVersion}", GUILayout.MinWidth(100f), GUILayout.Height(40f)) == true) {
+
+                    this.Close();
+
+                }
+                if (GUILayout.Button($"COMMIT AND UP VERSION TO {this.nextMajorMinorVersion}", GUILayout.MinWidth(100f), GUILayout.Height(40f)) == true) {
+
+                    this.majorMinorVersion = this.nextMajorMinorVersion;
+                    this.Close();
+
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+    
+            });
+            
+        }
+
+        private System.ReadOnlySpan<byte> GetBytes(string str) {
+            
+            return new System.ReadOnlySpan<byte>(System.Text.Encoding.UTF8.GetBytes(str));
+            
+        }
+
+        private void OnDestroy() {
+
+            this.Commit();
+
+        }
+        
+        private void Commit() {
+
+            if (string.IsNullOrEmpty(this.message) == false) {
+
+                var lines = this.changeLogFile.Split('\n').ToList();
+                var lineIndex = -1;
+                var versionFound = false;
+                var withPrefix = true;
+                for (int i = 0; i < lines.Count; ++i) {
+
+                    var line = lines[i];
+                    if (line.StartsWith("# ") == true) {
+
+                        if (versionFound == false) {
+
+                            // Read version
+                            var match = System.Text.RegularExpressions.Regex.Match(line, @"# Version ([0-9]+)\.([0-9]+)");
+                            if (match.Groups.Count > 0) {
+
+                                var major = match.Groups[1].Value;
+                                var minor = match.Groups[2].Value;
+                                if (this.majorMinorVersion == major + "." + minor) {
+
+                                    lineIndex = i;
+                                    versionFound = true;
+
+                                }
+
+                            }
+
+                        } else {
+
+                            break;
+
+                        }
+
+                    } else if (versionFound == true) {
+
+                        if (this.message.StartsWith(this.changedFilename) == true &&
+                            line.StartsWith(linePrefix + this.changedFilename) == true) {
+
+                            var resStr = this.message.Substring(this.changedFilename.Length, this.message.Length - this.changedFilename.Length);
+                            if (string.IsNullOrEmpty(resStr) == false) {
+
+                                var source = line.Substring(linePrefix.Length + this.changedFilename.Length, line.Length - this.changedFilename.Length - linePrefix.Length);
+                                if (string.IsNullOrEmpty(source) == false) {
+                                    lines[i] = "  * " + source;
+                                    lines.Insert(i, linePrefix + this.changedFilename);
+                                }
+
+                                this.message = "  * " + resStr;
+                                lineIndex = i;
+                                withPrefix = false;
+                                break;
+
+                            } else {
+
+                                lineIndex = -2;
+                                break;
+
+                            }
+
+                        }
+                        
+                    }
+
+                }
+
+                if (lineIndex >= 0) {
+
+                    if (withPrefix == true) {
+
+                        // line found
+                        lines.Insert(lineIndex + 1, linePrefix + this.message);
+
+                    } else {
+                        
+                        // line found
+                        lines.Insert(lineIndex + 1, this.message);
+
+                    }
+
+                } else if (lineIndex == -1) {
+
+                    // Add version header
+                    lines.Insert(0, "");
+                    lines.Insert(0, linePrefix + this.message);
+                    lines.Insert(0, $"# Version {this.majorMinorVersion}");
+
+                }
+
+                if (lineIndex >= -1) {
+
+                    var str = string.Join("\n", lines);
+                    System.IO.File.WriteAllText(this.changeLogFilePath, str);
+
+                }
+
+            }
+
+            //this.changeLogFile.Write(this.GetBytes(this.message));
+            //this.changeLogFile.Close();
+            this.callback?.Invoke(this.version);
+            
+        }
+
+    }
+
+}
