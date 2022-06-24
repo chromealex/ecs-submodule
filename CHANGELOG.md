321d15972aaf196ec32f6ef13d28401060ffc946
Alex Silaev <chrome.alex@gmail.com>
2022-06-24 13:52:58 +0300

    Pathfinding: PathNavMesh component changed from DataObject to ListCopyable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Version 2.5
+* Essentials.Pathfinding: NavMeshPath DataObject changed to ListCopyable
 * GOAPAction: fixed point math support added [Oleg-Grim]
 * GetNearest: distance dependant flag added [Oleg-Grim]
 * OverrideCost: implemented [Oleg-Grim]
--- Essentials/Pathfinding/Features/PathfindingNavMesh/Components/PathNavMesh.cs
+++ Essentials/Pathfinding/Features/PathfindingNavMesh/Components/PathNavMesh.cs
@@ -3,10 +3,20 @@ using ME.ECS.Mathematics;
 
 namespace ME.ECS.Pathfinding.Features.PathfindingNavMesh.Components {
 
-    public struct PathNavMesh : IComponent {
+    public struct PathNavMesh : ICopyable<PathNavMesh> {
 
         public ME.ECS.Pathfinding.PathCompleteState result;
-        public ME.ECS.Collections.NativeDataBufferArray<float3> path;
+        public ME.ECS.Collections.ListCopyable<float3> path;
+
+        public void CopyFrom(in PathNavMesh other) {
+            this.result = other.result;
+            ArrayUtils.Copy(other.path, ref this.path);
+        }
+
+        public void OnRecycle() {
+            this.result = default;
+            PoolListCopyable<float3>.Recycle(ref this.path);
+        }
 
     }
 
