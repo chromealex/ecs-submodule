e1e2f22be6ba46184960d6372beb19b5befc2982
Denis Petrenko <petrenko.den@gmail.com>
2022-07-02 21:25:00 +0300

    Essentials: DestroyByTime fixed-point support added

--- Essentials/Destroy/Components/DestroyByTime.cs
+++ Essentials/Destroy/Components/DestroyByTime.cs
@@ -1,10 +1,17 @@
-﻿using ME.ECS;
+﻿#if FIXED_POINT_MATH
+using ME.ECS.Mathematics;
+using tfloat = sfloat;
+#else
+using Unity.Mathematics;
+using tfloat = System.Single;
+#endif
+using ME.ECS;
 
 namespace ME.ECS.Essentials.Destroy.Components {
 
     public struct DestroyByTime : IComponent {
 
-        public float time;
+        public tfloat time;
 
     }
     
