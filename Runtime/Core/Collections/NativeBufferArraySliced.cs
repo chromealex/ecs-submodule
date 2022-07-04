46550575b0aabe1f53d2f951b96a627c0ede240a
Alex Silaev <chrome.alex@gmail.com>
2022-07-04 10:11:01 +0300

    NativeArrayUtils hotfix

--- Runtime/Core/Collections/NativeBufferArraySliced.cs
+++ Runtime/Core/Collections/NativeBufferArraySliced.cs
@@ -296,20 +296,6 @@ namespace ME.ECS.Collections {
 
         }
 
-        [System.Runtime.CompilerServices.MethodImplAttribute(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-        public NativeBufferArraySliced<T> Clamp<TCopy>(int length, TCopy copy) where TCopy : IArrayElementCopy<T> {
-
-            var newArr = this.Merge();
-            for (int i = length; i < this.Length; ++i) {
-                
-                copy.Recycle(ref newArr[i]);
-                
-            }
-            
-            return new NativeBufferArraySliced<T>(new NativeBufferArray<T>(newArr.data.arr, length));
-
-        }
-
     }
     #endif
 
