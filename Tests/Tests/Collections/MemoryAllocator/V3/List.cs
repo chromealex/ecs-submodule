f7d360751ab25078f420a5e0e74d699d2314f1ff
Alex Silaev <chrome.alex@gmail.com>
2022-07-21 13:54:17 +0300

    V3.List Tests: Clear added

--- Tests/Tests/Collections/MemoryAllocator/V3/List.cs
+++ Tests/Tests/Collections/MemoryAllocator/V3/List.cs
@@ -122,6 +122,26 @@ namespace ME.ECS.Tests.MemoryAllocator.V3 {
 
         }
 
+        [Test]
+        public void Clear() {
+
+            var allocator = Base.GetAllocator(10);
+
+            var list = new List<Vector3>(ref allocator, 10);
+            for (int i = 0; i < 100; ++i) {
+
+                list.Add(ref allocator, new Vector3(i, i, i));
+                
+            }
+            
+            Assert.IsTrue(list.Count == 100);
+            list.Clear();
+            Assert.IsTrue(list.Count == 0);
+
+            allocator.Dispose();
+
+        }
+
     }
 
 }
